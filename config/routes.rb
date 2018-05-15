548da349d032aa9e3ed5a7a1ea1a78cffcda0ae9
badbug wne <sweewen45@gmail.com>
2018-05-15 09:29:25 +0800

    set up routes (products, carts and orders resources)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :products, only: [:index, :show]
+  resources :carts, only: [:show]
+  resources :orders, only: [:show]
   root "products#index"
 
   namespace :admin do
+    resources :products
+    resources :orders
     root "products#index"
   end  
 

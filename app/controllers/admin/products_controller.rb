1552b80372a7a382eaff7061459d3c5c2ae23d00
badbug wne <sweewen45@gmail.com>
2018-05-17 09:47:30 +0800

    set up admin/products admin/orders view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,10 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+
+  def index
+    @products = Product.page(params[:page]).per(12)
+    
+  end
+  
 end

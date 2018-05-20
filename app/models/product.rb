323b7caec6855bf49360f1e683f32e542445644d
badbug wne <sweewen45@gmail.com>
2018-05-20 15:46:16 +0800

    all visitor can add product to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   mount_uploader :image, ProductImageUploader
   validates_presence_of :name, :description, :price
 
-  has_many :cartitems, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
-  has_many :orderitems, dependent: :destroy
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 end
